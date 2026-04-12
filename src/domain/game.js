b818f42ca6a343188964f83a8bf3364d40db4c88
Li Weizheng <liwzh69@mail2.sysu.edu.cn>
2026-04-12 18:44:55 +0800

    Remove import of BOX_SIZE and SUDOKU_SIZE constants

--- src/domain/game.js
+++ src/domain/game.js
@@ -1,5 +1,4 @@
 import { createSudokuFromJSON } from './sudoku.js'
-import { BOX_SIZE, SUDOKU_SIZE } from '../node_modules/@sudoku/constants.js'
 
 class Game {
   constructor(sudoku, history = { past: [], future: [] }) {
